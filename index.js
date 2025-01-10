5f554211e5e5cdc5900b064dc20464b5cfe0a971
Kyrillos Ibrahim <kyrillosibrahim@Kyrilloss-Air.home>
2025-01-09 22:50:19 -0500

    Updated the Node.JS test code using express

--- index.js
+++ index.js
@@ -1,31 +1,18 @@
-const http = require("http");
-const fs = require("fs");
-const url = require("url");
+const express = require("express");
+const app = express();
 
-const server = http.createServer((req, res) => {
-  let store = url.parse(req.url, true);
-  let pathname =
-    store.pathname === "/" ? "/index.html" : store.pathname + ".html"; // Default to index.html
-  let filename = "." + pathname.replace("//", "/"); // Ensure clean path
-
-  fs.readFile(filename, function (err, data) {
-    if (err) {
-      fs.readFile("404.html", function (err, data2) {
-        if (err) {
-          res.writeHead(404, { "Content-Type": "text/html" });
-          res.end("Error");
-        } else {
-          res.writeHead(404, { "Content-Type": "text/html" });
-          res.write(data2);
-          res.end();
-        }
-      });
-    } else {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(data);
-      res.end();
-    }
-  });
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+app.get("/about", (req, res) => {
+  res.sendFile(__dirname + "/about.html");
+});
+app.get("/contact-me", (req, res) => {
+  res.sendFile(__dirname + "/contact-me.html");
 });
 
-server.listen(8080);
+app.use((req, res) => {
+  res.status(404).sendFile(__dirname + "/404.html");
+});
+
+app.listen(8080);
